4c54fd89daf0fa6b7b63eb408037ec7b50e69b38
Maycon Ferreira <mayconferr.18@gmail.com>
2024-02-02 12:01:31 -0300

    Change time fetch expeditions

--- integration_tiny/settings.py
+++ integration_tiny/settings.py
@@ -152,13 +152,13 @@ CELERY_BEAT_SCHEDULE = {
         'task': 'core.tasks.task_update_orders',
         'schedule': crontab(minute='*/5'),
     },
-    # 'search-expeditions': {
-    #     'task': 'core.tasks.task_search_expeditions',
-    #     'schedule': crontab(minute='*/10'),
-    # },
+    'search-expeditions': {
+        'task': 'core.tasks.task_search_expeditions',
+        'schedule': crontab(minute='*/20'),
+    },
     'send-labels': {
         'task': 'core.tasks.task_send_labels',
-        'schedule': crontab(minute='*/10'),
+        'schedule': crontab(minute='*/5'),
     },
     'send-orders-awaiting-integration': {
         'task': 'core.tasks.task_send_orders_awaiting_integration',
